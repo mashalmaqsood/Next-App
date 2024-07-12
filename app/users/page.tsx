b1f14f274c8f8e9dd3848543d608fd8fd9b0a410
Mashal <Mashal.maqsood112@gmail.com>
2024-07-12 08:03:45 -0700

    initial commit.

---
+++ app/users/page.tsx
@@ -0,0 +1,20 @@
+import React from 'react'
+
+type User = {
+    id : number,
+    name : string
+}
+const UsersPage = async() => {
+    const res = await fetch("https://jsonplaceholder.typicode.com/users");
+    const users : User[] = await res.json();
+  return (
+    <div>
+      <h1 >Users</h1>
+       {
+        users.map(user => <li key={user.id}>{user.name}</li>)
+       }
+    </div>
+  )
+}
+
+export default UsersPage
